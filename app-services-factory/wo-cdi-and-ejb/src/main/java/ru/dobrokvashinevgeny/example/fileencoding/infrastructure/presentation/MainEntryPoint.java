10f1ecf1e7477a38561abae404f615f52805dffa
stalker <jenya_d1@mail.ru>
2018-06-18 22:52:49 +0300

    Fix name

--- app-services-factory/wo-cdi-and-ejb/src/main/java/ru/dobrokvashinevgeny/example/fileencoding/infrastructure/presentation/MainEntryPoint.java
+++ app-services-factory/wo-cdi-and-ejb/src/main/java/ru/dobrokvashinevgeny/example/fileencoding/infrastructure/presentation/MainEntryPoint.java
@@ -15,20 +15,20 @@ public class MainEntryPoint {
 
 	public static void main(String[] args) throws Exception { //<- точка входа
 		// Создание экземпляра фабрики сервисов приложения из ее конфигурации:
-		Application application = Applications.createFromConfigFileWithName(getConfigFileLocation());
+		Application application = Applications.createFromConfigFileWithName(getLocationConfigFile());
 		// Создание экземпляра прокси сервиса приложения из фабрики сервисов:
 		AppServiceProxy appServiceProxy = application.createAppServiceProxy();
 		// Выполнение нужного в данном месте сценария использования:
 		appServiceProxy.executeUseCase();
 	}
 
-	private static String getConfigFileLocation() throws GeneralApplicationException {
-		final String configFileLocation = System.getProperty(LOCATION_APP_CONFIG_FILE);
-		if (configFileLocation == null ||
-			"".equals(configFileLocation.trim())) {
+	private static String getLocationConfigFile() throws GeneralApplicationException {
+		final String locationConfigFile = System.getProperty(LOCATION_APP_CONFIG_FILE);
+		if (locationConfigFile == null ||
+			"".equals(locationConfigFile.trim())) {
 			throw new GeneralApplicationException("Parameter " + LOCATION_APP_CONFIG_FILE + " - not found!");
 		}
 
-		return configFileLocation;
+		return locationConfigFile;
 	}
 }
